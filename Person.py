56ac0fc77e85d1cada8e03b4bd17e3f737d0b658
JanTeffo442 <59921156+JanTeffo442@users.noreply.github.com>
2020-02-28 10:04:04 +0200

    Update Person.py

--- Person.py
+++ Person.py
@@ -7,7 +7,7 @@ class Person:
 
     def hello(self):
         interest = ', '.join(self.interests)
-        print(f'Hello, my name is {self.name} and I am {self.age} years old. My interests are {self.interests}')
+        print(f'Hello, my name is {self.name} and I am {self.age} years old. My interests are {interest}')
         return self.hello
 
 person = Person('Ryan', 30, 'male', ['being a hardarse', 'agile', 'ssd hard drives'])
