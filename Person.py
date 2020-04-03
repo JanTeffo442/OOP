d7a88b2da3193ab41c6e897cd58fb13412e58c10
JanTeffo442 <jan.teffo@umuzi.org>
2020-04-03 17:19:03 +0200

    reviewed commit

--- Person.py
+++ Person.py
@@ -7,8 +7,9 @@ class Person:
 
     def hello(self):
         interest = ', '.join(self.interests)
-        print(f'Hello, my name is {self.name} and I am {self.age} years old. My interests are {interest}')
-        return self.hello
+        return f'Hello, my name is {self.name} and I am {self.age} years old. My interests are {interest}'
+        #return self.hello
 
 person = Person('Ryan', 30, 'male', ['being a hardarse', 'agile', 'ssd hard drives'])
 greeting = person.hello()
+print(greeting)
